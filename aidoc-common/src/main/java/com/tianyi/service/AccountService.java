1f23489b7771cf6bae696f49140504fca3df7f85
vliu <v5100v5100@163.com>
2018-09-13 10:25:33 +0800

    Update AccountService.java
    重构AccountService

--- aidoc-common/src/main/java/com/tianyi/service/AccountService.java
+++ aidoc-common/src/main/java/com/tianyi/service/AccountService.java
@@ -1,181 +1,189 @@
-package com.tianyi.service;
+package me.aidoc.appserver.service.user;
 
-import com.tianyi.bo.Account;
-import com.tianyi.bo.AccountDetail;
-import com.tianyi.dao.AccountDao;
-import com.tianyi.dao.AccountDetailDao;
-import com.tianyi.mapper.AccountMapper;
-import com.tianyi.vo.AidocRankingVo;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import me.aidoc.appserver.common.PageData;
+import me.aidoc.appserver.common.exception.ServerBizException;
+import me.aidoc.appserver.po.user.Account;
+import me.aidoc.appserver.po.user.AccountFreeze;
+import java.math.BigDecimal;
 
 /**
- * Created by 雪峰 on 2018/1/16.
- */
-@Service("accountService")
-public class AccountService {
-    @Resource
-    AccountDao accountDao;
-    @Resource
-    AccountDetailDao accountDetailDao;
-    @Autowired
-    AccountMapper accountMapper;
+ * @author vliu
+ * @create 2018-08-16 15:57
+ **/
+public interface AccountService {
+
+    /**
+     *  操作结果:成功
+     */
+   int RESULT_SUCCESS = 0x01;
+
+    /**
+     * 操作结果:失败，用户ID不能为空
+     */
+    int RESULT_USERID_IS_NOT_NULL = 0x02;
+
+    /**
+     * 操作结果:失败，用户已经存在
+     */
+    int RESULT_ACCOUNT_HAS_EXIST = 0x03;
+
+    /**
+     * 操作结果:失败，用户不存在
+     */
+    int RESULT_ACCOUNT_NOT_EXIST = 0x04;
+
+    /**
+     * 操作结果:失败，用户状态异常
+     */
+    int RESULT_ACCOUNT_ABNORMAL = 0x05;
+
+    /**
+     * 操作结果:失败，余额不足
+     */
+    int RESULT_BALANCE_INSUFFICIENT = 0x06;
+    /**
+     * 操作结果:失败，businessNo错误
+     */
+    int RESULT_BUSINESS_NO_ERROR = 0x07;
+
+    /**
+     * 操作失败，没有任何更新
+     */
+    int RESULT_NO_UPDATE = 0xEF;
+
+    /**
+     * 操纵结果:失败，数据库错误
+     */
+    int RESULT_DATABASE_ERROR = 0xFF;
+
+    /**
+     * 账户状态:正常
+     */
+    int STATUS_NORMAL = 0;
+
+    /**
+     * 账户状态:冻结
+     */
+    int STATUS_FREEZE = 2;
+
+    /**
+     * 创建账户，返回值说明:
+     *      1.成功
+     *      2.失败，账户已存在
+     *      3.失败，余额不能为负数
+     *      4.失败，eth地址不合法
+     *      110.数据库连接错误
+     *
+     * @param createAccountInfoDTO
+     *
+     * @return
+     */
+    int createAccount(CreateAccountInfoDTO createAccountInfoDTO);
+
+    /**
+     * 更新用户的账户信息，返回值说明:
+     *     1.更新成功
+     *     2.失败，userId不能为空
+     *     3.失败，userId不存在
+     *     4.失败，不支持的status
+     *     110.数据库连接错误
+     *
+     * @param updateAccountInfoDTO
+     * @return
+     */
+    int updateAccount(UpdateAccountInfoDTO updateAccountInfoDTO);
+
+    /**
+     * 增加余额
+     * @return
+     */
+    int increaseBalance(AccountInOrOutDTO accountInOrOutDTO);
+
+    /**
+     * 减少余额
+     * @return
+     */
+    int reduceBalance(AccountInOrOutDTO accountInOrOutDTO);
+
+    /**
+     * 冻结余额
+     * @return
+     */
+    int freezeBalance(AccountFreezeDTO accountFreezeDTO);
+
+    /**
+     * 更新用户的冻结信息
+     * @param updateAccountFeezeDTO
+     *
+     * @return
+     */
+    int updateFreeze(UpdateAccountFeezeDTO updateAccountFeezeDTO);
 
 
+    /**
+     *
+     * @param userId         需要解冻用户的Id
+     * @param businessNo     业务号
+     * @param needGiveBack   是否需要归还冻结金额，慎重，根据具体业务自己确定
+     * @return
+     */
+    int unfreezeBalance(Long userId,String businessNo,boolean needGiveBack);
 
-    public Account getAccountByUserId(long userId){
-        return accountDao.getAccountByUserId(userId);
-    }
+    /**
+     * 获取用户的余额
+     * @param userId
+     * @return
+     */
+    BigDecimal getAccountBalance(Long userId);
 
-    public List<AccountDetail> getAccountDetails(final long userId , final int page, final int pageSize){
+    /**
+     *
+     * @param userId
+     * @return
+     */
+    Account getAccountByUserId(Long userId);
 
-        Account account = accountDao.getAccountByUserId(userId);
-        if(account ==null){
-            return null;
-        }
+    /**
+     * 根据eth地址查询账户
+     * @param ethAddress
+     * @return
+     */
+    Account getAccountByEthAddress(String ethAddress);
 
-        return accountDetailDao.getAccountDetails(account.getId(),page,pageSize);
-    }
-    public long getDayMoney(long userId,String day){
-        return accountDetailDao.getDayMoney(userId,day);
-    }
+    /**
+     * 校验支付密码是否正确
+     * @param userId         被校验者的userId
+     * @param payPassword    支付密码
+     * @return
+     */
+    boolean checkPayPassword(Long userId,String payPassword);
 
-    public long getStepsDayMoney(long userId,String day){
-        return accountDetailDao.getStepsDayMoney(userId,day);
-    }
+    /**
+     * 查询用户的收支明细
+     * @param userId
+     * @param page
+     * @param size
+     * @return
+     */
+    PageData getAccountDetailByUserId(Long userId, Integer page, Integer size);
 
-    //coinNum 支持正/负
-    //如果为消费类型需要变成负值
-    //发币专用因为发币详细日志中要把时间改为前一天
-    public long coin(long userId,String channel,long coin) {
+    /**
+     * 根据业务号查询冻结相关的信息
+     * @param businessNo
+     * @return
+     */
+    AccountFreeze getAccountFreeze(String businessNo);
 
-        if(coin == 0 ){
-            return 0;
-        }
+    /**
+     * 获取用户的冻结列表
+     * @param userId
+     * @return
+     */
+    PageData getAccountFreezeList(Long userId,Integer page,Integer size);
 
-        //判定用户是否而存在数据
-        Account account = accountDao.getAccountByUserId(userId);
-
-        if (coin < 0 && account != null && (coin + account.getBalance()) < 0) {
-            return (coin + account.getBalance());
-        }
-
-        if (account == null) {
-            account = saveAccount(userId, coin );
-        } else {
-            account = editAccount(account, coin);
-        }
-        //创建详细数据日志
-        addTblAccountDetail(account.getId(), coin, account.getBalance(),channel);
-        return 1;
-    }
-
-
-
-    private Account saveAccount(long userMainId, long coin) {
-        Account account = new Account();
-
-        account.setUserId(userMainId);
-        account.setRewardBalance(coin );
-        account.setRewardSum(coin );
-        account.setBalance(coin );
-        accountDao.add(account);
-        return account;
-    }
-
-
-    private Account editAccount(Account account, long coin) {
-        account.setRewardBalance(account.getRewardBalance() == null ? 0 : account.getRewardBalance() + coin );
-        if (coin > 0) {
-            account.setRewardSum(account.getRewardSum() == null ? 0 : account.getRewardSum() + coin );
-        }
-        account.setBalance(account.getBalance() == null ? 0 : account.getBalance() + coin );
-
-        accountDao.update(account);
-        return account;
-    }
-
-
-    private void addTblAccountDetail(long accountId, long rewardAmount, long blance, String channel) {
-        Calendar calendar=Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.DAY_OF_MONTH,-1);
-        calendar.set(Calendar.HOUR_OF_DAY,23);
-        calendar.set(Calendar.MINUTE,59);
-        calendar.set(Calendar.SECOND,59);
-        calendar.set(Calendar.MILLISECOND,0);
-        AccountDetail accountDetail = new AccountDetail();
-        accountDetail.setCreatedOn(calendar.getTime());
-        accountDetail.setUpdatedOn(calendar.getTime());
-        accountDetail.setUpdatedTimestamp(calendar.getTimeInMillis());
-        accountDetail.setAccountId(accountId);
-        accountDetail.setRewardAmount(rewardAmount );
-        accountDetail.setBalance(blance);
-        accountDetail.setChannel(channel);
-       accountDetailDao.addAccountDetail(accountDetail);
-    }
-
-    //coinNum 支持正/负
-    //如果为消费类型需要变成负值
-    public long consumeCoin(long userId,String channel,long coin,Date createOn) {
-
-        if(coin == 0 ){
-            return 0;
-        }
-
-        //判定用户是否而存在数据
-        Account account = accountDao.getAccountByUserId(userId);
-
-        if (coin < 0 && account != null && (coin + account.getBalance()) < 0) {
-            return (coin + account.getBalance());
-        }
-
-        if (account == null) {
-            account = saveAccount(userId, coin );
-        } else {
-            account = editAccount(account, coin);
-        }
-        //创建详细数据日志
-        addConsumeCoinDetail(account.getId(), coin, account.getBalance(),channel, createOn);
-        return 1;
-    }
-
-    private void addConsumeCoinDetail(long accountId, long rewardAmount, long blance, String channel,Date createOn) {
-        AccountDetail accountDetail = new AccountDetail();
-        accountDetail.setAccountId(accountId);
-        accountDetail.setRewardAmount(rewardAmount );
-        accountDetail.setBalance(blance);
-        accountDetail.setChannel(channel);
-        accountDetail.setCreatedOn(createOn);
-        accountDetail.setUpdatedOn(DateTime.now().toDate());
-        accountDetail.setUpdatedTimestamp(DateTime.now().toDate().getTime());
-        accountDetailDao.addAccountDetail(accountDetail);
-    }
-
-    public Account getAccountByUserId(Long userId){
-        return accountMapper.getAccountByUserId(userId);
-    }
-
-    public List<AidocRankingVo> getAllAidocRanking(Map param){
-        return accountMapper.getAllAidocRanking(param);
-    }
-
-    public int getAllAidocRankingCount(){
-        return accountMapper.getAllAidocRankingCount();
-    }
-
-    public List<AidocRankingVo> getFriendlyAidocRanking(Map param){
-        return accountMapper.getFriendlyAidocRanking(param);
-    }
-
-    public int getFriendlyAidocRankingCount(Map param){
-        return accountMapper.getFriendlyAidocRankingCount(param);
-    }
+    /**
+     * 给指定用户生成eth地址
+     * @param userId
+     */
+    void createEthAddress(Long userId) throws ServerBizException;
 }
